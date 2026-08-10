332ec47c245b00675aefc0b80fdc996d42dc4b9c
Your Name <your@email.com>
2026-08-10 15:24:59 +0545

    Intigrate Vercal analytic

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { TimerProvider } from './context/TimerContext'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TimerProvider>
       <App />
+      <Analytics />
     </TimerProvider>
   </React.StrictMode>,
 )
